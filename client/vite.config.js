2e945f988842afef5434dd96d32415b4a7d4423c
Underated-kiddo <lawrencebradley231@gmail.com>
2025-07-25 01:32:23 -0700

    fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',},
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
